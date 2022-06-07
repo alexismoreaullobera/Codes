84dc01ff3e014a3110e6ad56097f9dd37ee272af
alexismoreaulloberauser.email <alexis.moreau.llobera@gmail.com>
2022-06-07 02:08:52 +0200

    Ajout du premier model/vue/tempalte

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from listings.models import Topic
+from django.http import HttpResponse
 
 # Create your views here.
+
+def topic (request) :
+    topics = Topic.objects.all()
+    return render (request, 'listings/topic.html', {'first_topic': topics[0]})
